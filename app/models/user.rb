e5a7a3c9a0f9a65a2c55a83656913e2e2f4c138a
yanis khellaf <yanis.khellaf@outlook.fr>
2022-06-29 03:30:43 +0200

    ajout d'une photo utilisateur

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable
   devise :omniauthable, omniauth_providers: %i[facebook]
+  has_one_attached :photo
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
